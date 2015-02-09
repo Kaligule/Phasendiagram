42badf2d8dd401d69e20ae7f2e82b76c1d2cb2d2
Jøhannes Lippmann <code@schauderbasis.de>
2015-02-09 10:25:10 +0100

    fixed typo: ghcoutputdir -> ghc_outputdir

--- website.sh
+++ website.sh
@@ -2,7 +2,7 @@
 killall lighttpd
 
 # webserver.hs kompilieren (und webserver.cgi nennen, damit es von lighttpd gefunden wird)
-ghc --make -O2 -o webserver.cgi -outputdir ghcoutputdir webserver.hs
+ghc --make -O2 -o webserver.cgi -outputdir ghc_outputdir webserver.hs
 
 # einen neuen Webserver hinstellen mit der richtigen config
 lighttpd -f lighttpd.conf

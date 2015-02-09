8e0e175fa8c993925c8760298d16e181b833bff6
Jøhannes Lippmann <code@schauderbasis.de>
2015-02-09 08:38:33 +0100

    set outputdirectory for ghc so I won't have to deal with all the .o and .hi files.

--- website.sh
+++ website.sh
@@ -1,5 +1,8 @@
 # den alten Webserver wegwerfen
 killall lighttpd
 
+# webserver.hs kompilieren (und webserver.cgi nennen, damit es von lighttpd gefunden wird)
+ghc --make -O2 -o webserver.cgi -outputdir ghcoutputdir webserver.hs
+
 # einen neuen Webserver hinstellen mit der richtigen config
 lighttpd -f lighttpd.conf
